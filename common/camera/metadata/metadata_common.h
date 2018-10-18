android/platform_hardware_libhardware

camera: fix `-Wformat` compiler warnings
Test: add `USE_CAMERA_V4L2_HAL := true` to
      hardware/libhardware/modules/camera/3_4/Android.mk and build
      mmm hardware/libhardware/modules/camera/3_4
Change-Id: Ib1d1fc2de98436dd7e9d15ab324789725bb2002e
Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

modules/camera/3_4/metadata/metadata_common.h
@@ -180,7 +180,7 @@ static int SingleTagValue(const android::CameraMetadata& metadata,
   } else if (entry.count != 1) {
     HAL_LOGE(
         "Error: expected metadata tag %d to contain exactly 1 value "
-        "(had %d).",
+        "(had %zd).",
         tag,
         entry.count);
     return -EINVAL;
@@ -211,7 +211,7 @@ static int SingleTagValue(const android::CameraMetadata& metadata,
   } else if (entry.count != N) {
     HAL_LOGE(
         "Error: expected metadata tag %d to contain a single array of "
-        "exactly %d values (had %d).",
+        "exactly %zd values (had %zd).",
         tag,
         N,
         entry.count);
@@ -284,7 +284,7 @@ static int VectorTagValue(const android::CameraMetadata& metadata,
   if (entry.count % N != 0) {
     HAL_LOGE(
         "Error: expected metadata tag %d to contain a vector of arrays of "
-        "length %d (had %d entries, which is not divisible by %d).",
+        "length %zd (had %zd entries, which is not divisible by %zd).",
         tag,
         N,
         entry.count,

modules/camera/3_4/static_properties.cpp
@@ -277,11 +277,11 @@ bool StaticProperties::SanityCheckStreamConfiguration(
   for (size_t i = 0; i < stream_config->num_streams; ++i) {
     const camera3_stream_t* stream = stream_config->streams[i];
     if (stream == nullptr) {
-      ALOGE("%s: Stream %d is null", __func__, i);
+      ALOGE("%s: Stream %zd is null", __func__, i);
       return false;
     } else if (!IsInputType(stream->stream_type) &&
                !IsOutputType(stream->stream_type)) {
-      ALOGE("%s: Stream %d type %d is neither an input nor an output type",
+      ALOGE("%s: Stream %zu type %d is neither an input nor an output type",
             __func__,
             i,
             stream->stream_type);
@@ -321,7 +321,7 @@ bool StaticProperties::InputStreamsSupported(
   // Check the count.
   if (num_input_streams > max_input_streams_) {
     ALOGE(
-        "%s: Requested number of input streams %d is greater than "
+        "%s: Requested number of input streams %zd is greater than "
         "the maximum number supported by the device (%d).",
         __func__,
         num_input_streams,
@@ -406,7 +406,7 @@ bool StaticProperties::OutputStreamsSupported(
   if (num_raw > max_raw_output_streams_) {
     ALOGE(
         "%s: Requested stream configuration exceeds maximum supported "
-        "raw output streams %d (requested %d).",
+        "raw output streams %d (requested %zd).",
         __func__,
         max_raw_output_streams_,
         num_raw);
@@ -414,7 +414,7 @@ bool StaticProperties::OutputStreamsSupported(
   } else if (num_stalling > max_stalling_output_streams_) {
     ALOGE(
         "%s: Requested stream configuration exceeds maximum supported "
-        "stalling output streams %d (requested %d).",
+        "stalling output streams %d (requested %zu).",
         __func__,
         max_stalling_output_streams_,
         num_stalling);
@@ -422,7 +422,7 @@ bool StaticProperties::OutputStreamsSupported(
   } else if (num_non_stalling > max_non_stalling_output_streams_) {
     ALOGE(
         "%s: Requested stream configuration exceeds maximum supported "
-        "non-stalling output streams %d (requested %d).",
+        "non-stalling output streams %d (requested %zu).",
         __func__,
         max_non_stalling_output_streams_,
         num_non_stalling);

modules/camera/3_4/v4l2_camera_hal.cpp
@@ -115,7 +115,7 @@ V4L2CameraHAL::~V4L2CameraHAL() {
 }
 
 int V4L2CameraHAL::getNumberOfCameras() {
-  HAL_LOGV("returns %d", mCameras.size());
+  HAL_LOGV("returns %zd", mCameras.size());
   return mCameras.size();
 }
 

afirago/mpsoc-android_device_xilinx

common/camera/metadata/metadata_common.h
@@ -180,7 +180,7 @@ static int SingleTagValue(const android::CameraMetadata& metadata,
   } else if (entry.count != 1) {
     HAL_LOGE(
         "Error: expected metadata tag %d to contain exactly 1 value "
-        "(had %d).",
+        "(had %zu).",
         tag,
         entry.count);
     return -EINVAL;
@@ -211,7 +211,7 @@ static int SingleTagValue(const android::CameraMetadata& metadata,
   } else if (entry.count != N) {
     HAL_LOGE(
         "Error: expected metadata tag %d to contain a single array of "
-        "exactly %d values (had %d).",
+        "exactly %zu values (had %zu).",
         tag,
         N,
         entry.count);
@@ -284,7 +284,7 @@ static int VectorTagValue(const android::CameraMetadata& metadata,
   if (entry.count % N != 0) {
     HAL_LOGE(
         "Error: expected metadata tag %d to contain a vector of arrays of "
-        "length %d (had %d entries, which is not divisible by %d).",
+        "length %zu (had %zu entries, which is not divisible by %zu).",
         tag,
         N,
         entry.count,

common/camera/static_properties.cpp
@@ -277,11 +277,11 @@ bool StaticProperties::SanityCheckStreamConfiguration(
   for (size_t i = 0; i < stream_config->num_streams; ++i) {
     const camera3_stream_t* stream = stream_config->streams[i];
     if (stream == nullptr) {
-      ALOGE("%s: Stream %d is null", __func__, i);
+      ALOGE("%s: Stream %zu is null", __func__, i);
       return false;
     } else if (!IsInputType(stream->stream_type) &&
                !IsOutputType(stream->stream_type)) {
-      ALOGE("%s: Stream %d type %d is neither an input nor an output type",
+      ALOGE("%s: Stream %zu type %d is neither an input nor an output type",
             __func__,
             i,
             stream->stream_type);
@@ -321,7 +321,7 @@ bool StaticProperties::InputStreamsSupported(
   // Check the count.
   if (num_input_streams > max_input_streams_) {
     ALOGE(
-        "%s: Requested number of input streams %d is greater than "
+        "%s: Requested number of input streams %zu is greater than "
         "the maximum number supported by the device (%d).",
         __func__,
         num_input_streams,
@@ -406,7 +406,7 @@ bool StaticProperties::OutputStreamsSupported(
   if (num_raw > max_raw_output_streams_) {
     ALOGE(
         "%s: Requested stream configuration exceeds maximum supported "
-        "raw output streams %d (requested %d).",
+        "raw output streams %d (requested %zu).",
         __func__,
         max_raw_output_streams_,
         num_raw);
@@ -414,7 +414,7 @@ bool StaticProperties::OutputStreamsSupported(
   } else if (num_stalling > max_stalling_output_streams_) {
     ALOGE(
         "%s: Requested stream configuration exceeds maximum supported "
-        "stalling output streams %d (requested %d).",
+        "stalling output streams %d (requested %zu).",
         __func__,
         max_stalling_output_streams_,
         num_stalling);
@@ -422,7 +422,7 @@ bool StaticProperties::OutputStreamsSupported(
   } else if (num_non_stalling > max_non_stalling_output_streams_) {
     ALOGE(
         "%s: Requested stream configuration exceeds maximum supported "
-        "non-stalling output streams %d (requested %d).",
+        "non-stalling output streams %d (requested %zu).",
         __func__,
         max_non_stalling_output_streams_,
         num_non_stalling);

common/camera/v4l2_camera_hal.cpp
@@ -115,7 +115,7 @@ V4L2CameraHAL::~V4L2CameraHAL() {
 }
 
 int V4L2CameraHAL::getNumberOfCameras() {
-  HAL_LOGV("returns %d", mCameras.size());
+  HAL_LOGV("returns %zu", mCameras.size());
   return mCameras.size();
 }
 